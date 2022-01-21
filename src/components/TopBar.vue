80607b7d6198766d78865b7e5718fef64252b47c
Ben <benjamin.schulz94@gmail.com>
2022-01-21 18:55:16 +1030

    Fix issue in router

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -43,7 +43,7 @@ export default {
 	},
 	computed: {
 		currentRouteName() {
-			return this.$route.name;
+			return router.currentRoute.name;
 		},
 	},
 	data() {
--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -1,20 +1,10 @@
 <template>
 	<v-main>
-		<!-- <section id="docs">
-			<v-parallax src="@/assets/tatooine.jpg" height="300">
-				<v-layout column align-center justify-center class="white--text">
-					<h1 class="white--text mb-2 display-1 text-center">Documentation</h1>
-				</v-layout>
-			</v-parallax>
-		</section> -->
-
-		<v-col cols="12" class="ma-0 pa-0">
-			<v-row fill-height>
-				<v-col cols="2" class="ma-0 pa-0">
-					<ToolSidebar />
-				</v-col>
-			</v-row>
-		</v-col>
+		<v-row fill-height>
+			<v-col cols="2" class="ma-0 pa-0">
+				<ToolSidebar />
+			</v-col>
+		</v-row>
 	</v-main>
 </template>
 
