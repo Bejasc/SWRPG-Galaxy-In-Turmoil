de475b83c8ce0c731d3cb61f968ead436a801ee2
Ben <benjamin.schulz94@gmail.com>
2022-04-02 12:20:36 +1030

    remove old unused code

--- src/plugins/StarWarsNameGen.ts
+++ src/plugins/StarWarsNameGen.ts
@@ -2,19 +2,6 @@ import axios from "axios";
 
 export type StarWarsNametype = "Male" | "Female" | "Twilek" | "Rodian" | "Trandoshan" | "Wookiee" | "Droid" | "Hutt" | "Duros" | "TrooperTK";
 
-function padLeadingZeros(num: string, size: number): string {
-	let s = num + "";
-	while (s.length < size) s = "0" + s;
-	return s;
-}
-
-export function getRandomTrooperDesignation(type: StarWarsNametype): string {
-	const rnum = Math.floor(Math.random() * 9999);
-	const id = padLeadingZeros(rnum.toString(), 4);
-
-	return `${type}-${id}`;
-}
-
 export async function generateRandomName(type: StarWarsNametype, num = 1, includeLastName = true): Promise<string> {
 	const swrpgApi: string = process.env.VUE_APP_SWRPG_API ?? "NOT PROVIDED";
 
